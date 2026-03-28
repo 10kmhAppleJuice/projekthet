1bc2a2606a0c198c7cc9325f215ffed6ec4ee9dd
10kmhAppleJuice <23d_bodis@nyirszikszi.hu>
2026-03-28 14:39:09 +0100

    csak a HTML fájlok + képek,stb
    csak a HTML fájlok + képek ,stb

---
+++ assets/js/main.js
@@ -0,0 +1,113 @@
+/*
+	Binary by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+
+document.querySelectorAll('.faq-question').forEach(question => {
+	question.addEventListener('click', () => {
+    	const answer = question.nextElementSibling;
+
+    	question.classList.toggle('active');
+    	answer.classList.toggle('open');
+  	});
+});
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const elements = document.querySelectorAll(".reveal-up");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+                observer.unobserve(entry.target); // csak egyszer animál
+            }
+        });
+    }, { threshold: 0.2 });
+
+    elements.forEach(el => observer.observe(el));
+});
+
+
+(function($) {
+
+	skel.breakpoints({
+		xlarge:	'(max-width: 1680px)',
+		large:	'(max-width: 1280px)',
+		medium:	'(max-width: 980px)',
+		small:	'(max-width: 736px)',
+		xsmall:	'(max-width: 480px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body'),
+			$header = $('#header');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Fix: IE.
+			if (skel.vars.browser == 'ie')
+				$body.addClass('is-ie');
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Scrolly.
+			$('.scrolly').scrolly({
+				speed: 1000,
+				offset: $header.outerHeight() -1
+			});
+
+		// Menu.
+			$('#menu')
+				.append('<a href="#menu" class="close"></a>')
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right'
+				});
+
+		// Posts.
+			var $posts = $('.post');
+
+			$posts.each(function() {
+
+				var $this = $(this),
+					$image = $this.find('.image'), $img = $image.find('img'),
+					x;
+
+				// Set image.
+					$image.css('background-image', 'url(' + $img.attr('src') + ')');
+
+				// Set position.
+					if (x = $img.data('position'))
+						$image.css('background-position', x);
+
+			});
+
+	});
+
+})(jQuery);
